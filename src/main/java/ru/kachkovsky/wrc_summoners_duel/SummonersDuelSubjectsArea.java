e60146acdf387f54e807fbb69346a5e8960effb9
Igor Kachkovsky <kachkovsky-noreply-spam@yandex.ru>
2019-05-08 00:52:56 +0400

    Implement Buy and Beat Stage

--- src/main/java/ru/kachkovsky/wrc_summoners_duel/SummonersDuelSubjectsArea.java
+++ src/main/java/ru/kachkovsky/wrc_summoners_duel/SummonersDuelSubjectsArea.java
@@ -8,6 +8,7 @@ import ru.kachkovsky.wrc.team.SubjectTeamAreaDeterminator;
 import ru.kachkovsky.wrc.winrate.DefaultWinRateComparator;
 import ru.kachkovsky.wrc.winrate.WinRate;
 import ru.kachkovsky.wrc_summoners_duel.player.Player;
+import ru.kachkovsky.wrc_summoners_duel.stage.SDBeatStage;
 import ru.kachkovsky.wrc_summoners_duel.stage.SDBuyStage;
 import ru.kachkovsky.wrc_summoners_duel.stage.SDFirstBeatStage;
 
@@ -20,9 +21,9 @@ public class SummonersDuelSubjectsArea implements SubjectsArea {
     private static SimpleAriaStaticContents STATIC_CONTENTS = new SimpleAriaStaticContents();
     private static SummonersDuelTeamDeterminator TEAM_DETERMINATOR = new SummonersDuelTeamDeterminator();
 
-    private static SDFirstBeatStage FIRST_STAGE = new SDFirstBeatStage();
-    private static SDBuyStage BEAT_STAGE = new SDBuyStage(null);
     private static SDBuyStage BUY_STAGE = new SDBuyStage(null);
+    private static SDBeatStage BEAT_STAGE = new SDBeatStage(null);
+    private static SDFirstBeatStage FIRST_STAGE = new SDFirstBeatStage(BEAT_STAGE, BUY_STAGE);
     private Player[] teams;
     private int currentPlayerIndex;
     private int currentPlayerUnitIndex;
@@ -34,7 +35,7 @@ public class SummonersDuelSubjectsArea implements SubjectsArea {
         this.currentPlayerUnitIndex = currentPlayerUnitIndex;
         if (turnStarted) {
             this.nextStage = FIRST_STAGE;
-        } else if (currentPlayerUnitIndex == teams[currentPlayerIndex].getUnits().size()) {
+        } else if (isBuyStage()) {
             this.nextStage = BUY_STAGE;
         } else {
             this.nextStage = BEAT_STAGE;
@@ -89,5 +90,9 @@ public class SummonersDuelSubjectsArea implements SubjectsArea {
 
     public int reversePlayerIndex() {
         return currentPlayerIndex ^ 1;
+    }
+
+    public boolean isBuyStage() {
+        return currentPlayerUnitIndex == teams[currentPlayerIndex].getUnits().size();
     }
 }
--- src/main/java/ru/kachkovsky/wrc_summoners_duel/stage/SDFirstBeatStage.java
+++ src/main/java/ru/kachkovsky/wrc_summoners_duel/stage/SDFirstBeatStage.java
@@ -1,20 +1,28 @@
 package ru.kachkovsky.wrc_summoners_duel.stage;
 
 import ru.kachkovsky.wrc.stage.Action;
-import ru.kachkovsky.wrc.stage.FinishCheck;
 import ru.kachkovsky.wrc.stage.Stage;
 import ru.kachkovsky.wrc_summoners_duel.SummonersDuelSubjectsArea;
 
 import java.util.List;
 
 public class SDFirstBeatStage extends Stage<SummonersDuelSubjectsArea> {
-    public SDFirstBeatStage() {
+    private SDBeatStage innerBeatStage;
+    private SDBuyStage innerBuyStage;
+
+    public SDFirstBeatStage(SDBeatStage innerBeatStage, SDBuyStage innerBuyStage) {
         //TODO: add checks for win now
         super(null);
+        this.innerBeatStage = innerBeatStage;
+        this.innerBuyStage = innerBuyStage;
     }
 
     @Override
     public List<Action<SummonersDuelSubjectsArea>> getActions(SummonersDuelSubjectsArea area) {
-        return null;
+        if (area.isBuyStage()) {
+            return innerBuyStage.getActions(area);
+        } else {
+            return innerBeatStage.getActions(area);
+        }
     }
 }
