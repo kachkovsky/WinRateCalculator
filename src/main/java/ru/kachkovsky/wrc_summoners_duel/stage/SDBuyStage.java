a50cbe9d3c1615e5c81cfab438786c05cbc808e5
Igor Kachkovsky <kachkovsky-noreply-spam@yandex.ru>
2019-05-11 00:50:17 +0400

    Implement units buy

--- src/main/java/ru/kachkovsky/wrc_summoners_duel/stage/SDBuyStage.java
+++ src/main/java/ru/kachkovsky/wrc_summoners_duel/stage/SDBuyStage.java
@@ -21,39 +21,67 @@ public class SDBuyStage extends Stage<SummonersDuelSubjectsArea> {
     @Override
     public List<Action<SummonersDuelSubjectsArea>> getActions(SummonersDuelSubjectsArea area) {
         int mp = area.getTeams()[area.getCurrentPlayerIndex()].getMp();
-        return createUnitsActionsRecursive(Collections.emptyList(), mp, mp);
-    }
-
-    //TODO: handle enemy splash
-    private List<Action<SummonersDuelSubjectsArea>> createUnitsActionsRecursive(List<Unit> createdUnitList, int mp, int maxMpForOneUnit) {
-        for (int i = mp; i > 0; i--) {
-            //TODO: implement buy
-            //createUnitsActionsRecursive(Collections.emptyList(),);
+        List<List<Unit>> unitsCombination = createUnitsUsingCurrentMp(new ArrayList<>(), mp, mp);
+        List<Action<SummonersDuelSubjectsArea>> list = new ArrayList<>();
+        for (List<Unit> units : unitsCombination) {
+            list.add(new BuyAction(units));
         }
-        return Collections.singletonList(new BuyAction(new ArrayList<>()));
+        list.add(new BuyAction(Collections.emptyList()));
+        return list;
     }
 
-    //private List<Action<SummonersDuelSubjectsArea>> createUnitsVariationWithCurrentMp(List<Unit> createdUnitList, int mp) {
+    private List<List<Unit>> createUnitsUsingCurrentMp(List<List<Unit>> createdUnitListVariation, int mp, int maxMpForOneUnit) {
+        List<List<Unit>> resultList = new ArrayList<>();
+        for (int i = maxMpForOneUnit; i > 0; i--) {
+            List<List<Unit>> list = unitsListIntoMultipleCombinationOfUnitLists(oneUnitCombinationForMp(i), createdUnitListVariation);
+            resultList.addAll(createUnitsUsingCurrentMp(list, mp - i, Math.min(i, mp - i)));
+            resultList.addAll(list);
+        }
+        return resultList;
+    }
 
     //max mp is 20
-    List<Unit> unitsCombinationForMp(int mp) {
+    private List<Unit> oneUnitCombinationForMp(int mp) {
         List<Unit> list = new ArrayList<>((mp > 0 ? MathUtils.powI(3, mp - 1) : 0) + (mp > 2 ? MathUtils.powI(3, mp - 3) : 0));
         if (mp > 0) {
             if (mp > 2) {
-                genUnitsForMp(list, true, 1, 0, 1, mp - 3);
+                genOneUnitForMp(list, true, 1, 0, 1, mp - 3, true);
             }
-            genUnitsForMp(list, false, 1, 0, 1, mp - 1);
+            genOneUnitForMp(list, false, 1, 0, 1, mp - 1, true);
         }
         return list;
     }
 
-    private void genUnitsForMp(List<Unit> unitVariations, boolean splash, int atk, int def, int hp, int mp) {
+    private void genOneUnitForMp(List<Unit> unitVariations, boolean splash, int atk, int def, int hp, int mp, boolean withAtk) {
         if (mp > 0) {
-            genUnitsForMp(unitVariations, splash, atk + 1, def, hp, mp - 1);
-            genUnitsForMp(unitVariations, splash, atk, def + 1, hp, mp - 1);
-            unitVariations.add(UnitUtils.createUnit(splash, atk, def, hp + mp));
+            if (withAtk) {
+                genOneUnitForMp(unitVariations, splash, atk + 1, def, hp, mp - 1, true);
+            }
+            genOneUnitForMp(unitVariations, splash, atk, def + 1, hp, mp - 1, false);
+            unitVariations.add(UnitUtils.createUnit(splash, atk, def, hp + mp * 2));
         } else {
             unitVariations.add(UnitUtils.createUnit(splash, atk, def, hp));
         }
+    }
+
+    private static List<List<Unit>> unitsListIntoMultipleCombinationOfUnitLists(List<Unit> unitList, List<List<Unit>> unitListCombination) {
+        List<List<Unit>> resultList = new ArrayList<>();
+        if (unitListCombination.isEmpty()) {
+            resultList.addAll(unitListCombination);
+            for (Unit unit : unitList) {
+                resultList.add(Collections.singletonList(unit));
+            }
+        } else {
+
+            for (Unit unit : unitList) {
+                for (List<Unit> units : unitListCombination) {
+                    ArrayList<Unit> list = new ArrayList<>(units.size() + 1);
+                    list.addAll(units);
+                    list.add(unit);
+                    resultList.add(list);
+                }
+            }
+        }
+        return resultList;
     }
 }
