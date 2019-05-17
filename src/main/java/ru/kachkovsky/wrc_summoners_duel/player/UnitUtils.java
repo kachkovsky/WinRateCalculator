3e71dd175881d535c155f9d31d7b593d5119d85a
Igor Kachkovsky <kachkovsky-noreply-spam@yandex.ru>
2019-05-17 19:57:49 +0400

    temp commit for SeondAndMoreTurnFinishCheck

--- src/main/java/ru/kachkovsky/wrc_summoners_duel/player/UnitUtils.java
+++ src/main/java/ru/kachkovsky/wrc_summoners_duel/player/UnitUtils.java
@@ -1,5 +1,7 @@
 package ru.kachkovsky.wrc_summoners_duel.player;
 
+import ru.kachkovsky.utils.MathUtils;
+
 import java.util.List;
 
 public class UnitUtils {
@@ -10,6 +12,19 @@ public class UnitUtils {
 
     public static Unit createAfterAttack(Unit u, int atk) {
         return new Unit(u.getAtk(), u.getDef(), u.getHp() - Math.max(0, atk - u.getDef()), u.hasSplash());
+    }
+
+    public static int findSplashUnitIndex(List<Unit> units) {
+        return findSplashUnitIndex(units, 0);
+    }
+
+    public static int findSplashUnitIndex(List<Unit> units, final int startIndex) {
+        for (int i = startIndex; i < units.size(); i++) {
+            if (units.get(i).hasSplash()) {
+                return i;
+            }
+        }
+        return MathUtils.INDEX_NOT_FOUND;
     }
 
     public static String unitsToString(List<Unit> units) {
--- src/main/java/ru/kachkovsky/wrc_summoners_duel/stage/finish_check/DSHeuristicNextTurnFinishCheck.java
+++ src/main/java/ru/kachkovsky/wrc_summoners_duel/stage/finish_check/DSHeuristicNextTurnFinishCheck.java
@@ -1,5 +1,6 @@
 package ru.kachkovsky.wrc_summoners_duel.stage.finish_check;
 
+import ru.kachkovsky.utils.MathUtils;
 import ru.kachkovsky.wrc.winrate.WinRate;
 import ru.kachkovsky.wrc_summoners_duel.SummonersDuelSubjectsArea;
 import ru.kachkovsky.wrc_summoners_duel.player.Player;
@@ -22,10 +23,14 @@ public class DSHeuristicNextTurnFinishCheck extends DSWinNowCheck {
             Player curTeam = area.getTeams()[indexToWin];
             Player otherTeam = area.getTeams()[indexToLose];
 
+            int splashUnitIndex = UnitUtils.findSplashUnitIndex(otherTeam.getUnits());
+            if (splashUnitIndex == MathUtils.INDEX_NOT_FOUND) {
+                curTeam.getUnits().size() + curTeam.getMp()> otherTeam.getUnits().size() + otherTeam.getHp()
+            }
 
 
             //return UnitUtils.summaryAttack(curTeam.getUnits()) >= otherTeam.getHp();
         }
-        return super.checkTeamsWinRate(area);
+        return null;
     }
 }
