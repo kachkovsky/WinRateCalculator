9e66f6bff852aef651b4f0ea94264f77b13defaa
Igor Kachkovsky <kachkovsky-noreply-spam@yandex.ru>
2019-07-06 00:48:12 +0400

    temporary rework main

--- src/main/java/ru/kachkovsky/Main.java
+++ src/main/java/ru/kachkovsky/Main.java
@@ -1,17 +1,47 @@
 package ru.kachkovsky;
 
 import ru.kachkovsky.wrc.eventsgraph.EventGraphNode;
+import ru.kachkovsky.wrc.stage.Action;
+import ru.kachkovsky.wrc.winrate.WinRate;
+import ru.kachkovsky.wrc.winrate.calculator.WinRateListForTeamCalculator;
+import ru.kachkovsky.wrc.winrate.calculator.WinRateListFullCalculator;
 import ru.kachkovsky.wrc_console_ui.ConsoleUI;
 import ru.kachkovsky.wrc_summoners_duel.SummonersDuelSubjectsArea;
 import ru.kachkovsky.wrc_summoners_duel.SummonersDuelSubjectsAreaFactory;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(8000);
 
-        SummonersDuelSubjectsArea area = SummonersDuelSubjectsAreaFactory.createNewGameArea(2);
+        SummonersDuelSubjectsArea area = SummonersDuelSubjectsAreaFactory.createNewGameArea(3);
+
         EventGraphNode<SummonersDuelSubjectsArea> node = new EventGraphNode<>(area, area.getNextStage());
+
         ConsoleUI consoleUI = new ConsoleUI();
-        consoleUI.uiForFullGame(node);
+
+//        consoleUI.uiForFullGame(node);
+//        System.exit(0);
+        int i = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        WinRateListForTeamCalculator calculator = new WinRateListForTeamCalculator();
+        Map<Action<SummonersDuelSubjectsArea>, EventGraphNode<SummonersDuelSubjectsArea>> actionEventGraphNodeMap;
+        while ((actionEventGraphNodeMap = node.calcWinRate()) != null) {
+            Map<Action<SummonersDuelSubjectsArea>, List<WinRate>> actionListMap = calculator.eventGraphMapToWinRateMapOnlyOneTeam(node.calcWinRate(), area);
+            System.out.println("$$$Game calculated$$$");
+            for (Map.Entry<Action<SummonersDuelSubjectsArea>, List<WinRate>> actionListEntry : actionListMap.entrySet()) {
+                i++;
+                consoleUI.printAction(i, actionListEntry.getKey(), "");
+                consoleUI.printWinRateList(actionListEntry.getValue(), "");
+            }
+            node = consoleUI.uiForTurn(scanner, node);
+        }
+
+        consoleUI.printWinRateList(node.getTeamsWinRate(), "FIN ");
     }
 }
